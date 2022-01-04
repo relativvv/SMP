ebbfe4ed25ffd5249dc028843ab8f92a1e6ccc21
relativvv <52501124+relativvv@users.noreply.github.com>
2022-01-04 18:03:01 +0100

    initial commit

---
+++ src/main/java/de/relativv/smp/main/Smp.java
@@ -0,0 +1,19 @@
+package de.relativv.smp.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Smp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    
+}
